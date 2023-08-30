ada4625ed80101b5725e866cc920bf925ec0ea32
FX Scripts <143389639+FXscripts1@users.noreply.github.com>
2023-08-30 15:57:00 +0200

    Add files via upload

---
+++ fx_greenzones/README.md
@@ -0,0 +1,7 @@
+# FX_Greenzones
+
+Greenzones / Restricted Zones for FiveM. Disable shooting, slow down cars, and more!
+
+Discord Donwload: https://discord.gg/N4sezScKsE
+
+
---
+++ fx_greenzones/fxmanifest.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+}
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
